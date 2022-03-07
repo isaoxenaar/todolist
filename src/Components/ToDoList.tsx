d0bb77ce483536d3a03b844cced49f75c1d1edfc
Isa Oxenaar <email@gmail.com>
2022-03-07 17:03:28 +0100

    complete app

--- src/Components/ToDoList.tsx
+++ src/Components/ToDoList.tsx
@@ -35,8 +35,10 @@ const ToDoList: FC<IProps>= (props) => {
         })
         const id: string = `${el.id}`
         const oldItem = JSON.parse(localStorage.getItem(id)  || '');
+        console.log("donehandler" + oldItem.done)
         oldItem.done = !oldItem.done;
-        localStorage.setItem(id, oldItem);   
+        console.log("donehandler" + oldItem.done)
+        localStorage.setItem(id, JSON.stringify(oldItem));
         setToDoList(newToDo);
     }
 
